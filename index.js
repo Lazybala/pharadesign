d02fc0ee359cd6fb3ff956d2c888969e3f22ddfb
Lazybala <115776732+Lazybala@users.noreply.github.com>
2023-01-30 22:27:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+var s="lo"
+function oc(){
+
+    if(s=="lo"){
+    document.getElementById("view").style.display="block"
+    s="ki"
+    }
+    else{
+        document.getElementById("view").style.display="none"
+        s="lo"
+    }
+}
